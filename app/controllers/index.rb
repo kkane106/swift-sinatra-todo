315062d9ca6382da3a8e7c892b378734e989206c
kkane106 <kkane106@gmail.com>
2015-06-29 14:55:46 -0400

    Move sushi json to controller index for testing

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,8 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/sushi.json'  do
+	content_type :json
+	return {:sushi => ["Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
+end
