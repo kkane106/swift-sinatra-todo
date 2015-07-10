40ca64b293fd1f3e0e49666c27203cdd386bac0e
kkane106 <kkane106@gmail.com>
2015-07-10 18:52:11 -0400

    update JSON with additional value for test

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,8 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
 get '/sushi.json'  do
 	content_type :json
-	return {:sushi => ["Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
+	return {:sushi => ["Banana", "Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
 end
