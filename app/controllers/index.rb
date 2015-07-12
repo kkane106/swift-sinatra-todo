c77a27c4baf6b9d47fa53858124bbd3160de9397
kkane106 <kkane106@gmail.com>
2015-07-12 02:01:02 -0400

    Add login routes with json response handling

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,3 +6,16 @@ get '/sushi.json'  do
 	content_type :json
 	return {:sushi => ["Banana", "Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
 end
+
+get '/nothing_to_see' do
+  "Nothing to see here. Move along."
+end
+
+post '/login' do
+  values = JSON.parse(request.env["rack.input"].read)
+  if values["username"]=='jameson' && values["password"]=='password'
+    "{\"success\":1}"
+  else
+    "{\"success\":0}"
+  end
+end
