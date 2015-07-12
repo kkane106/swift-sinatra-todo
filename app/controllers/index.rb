a3df1301ebb34f9a44c5fb808cf8a6ae714cc36d
kkane106 <kkane106@gmail.com>
2015-07-12 02:18:54 -0400

    continue investigating returns

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -14,7 +14,7 @@ end
 post '/login' do
   values = JSON.parse(request.env["rack.input"].read)
   if values["todos"]
-    "#{values["todos"]} \n just raw json: #{values}"
+    "#{values}"
   else
     "{\"success\":0}"
   end
