ce660e027ffcc7c908aa882ebbfcbcba67d27fe9
kkane106 <kkane106@gmail.com>
2015-07-12 03:00:23 -0400

    edit json data to test refresh button

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,7 +4,7 @@ end
 
 get '/sushi.json'  do
 	content_type :json
-	return {:sushi => ["Banana", "Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
+	return {:sushi => ["Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
 end
 
 get '/nothing_to_see' do
