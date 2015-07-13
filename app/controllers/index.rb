1f853b4d37bf7893cf423abbaca3049a30f5a7e0
kkane106 <kkane106@gmail.com>
2015-07-13 18:29:42 -0400

    Create todos_controller.rb for better organization

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -33,26 +33,3 @@ post "/delete_todo" do
     return "error".to_json
   end
 end
-
-get "/todos" do
-  @todos = Todo.all
-  erb :todos
-end
-
-get "/new" do
-  erb :new
-end
-
-post "/new" do
-  @todo = Todo.new(params[:todo])
-  if @todo.save
-    redirect "todo/#{@todo.id}"
-  else
-    erb :new
-  end
-end
-
-get "/todo/:id" do
-  @todo = Todo.find_by_id(params[:id])
-  erb :todo
-end
---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,22 @@
+get "/todos" do
+  @todos = Todo.all
+  erb :todos
+end
+
+get "/new" do
+  erb :new
+end
+
+post "/new" do
+  @todo = Todo.new(params[:todo])
+  if @todo.save
+    redirect "todo/#{@todo.id}"
+  else
+    erb :new
+  end
+end
+
+get "/todo/:id" do
+  @todo = Todo.find_by_id(params[:id])
+  erb :todo
+end
