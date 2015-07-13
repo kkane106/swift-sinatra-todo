c7342b6719a32c32fca9b191b6812d870ae724de
kkane106 <kkane106@gmail.com>
2015-07-13 16:56:12 -0400

    Try some JS

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+$(document).ready(
+  // Add Ajax to refresh Todos on /todos for reasons
+ function() {
+                setInterval(function() {
+                    var randomnumber = Math.floor(Math.random() * 100);
+                    $('#show').text(
+                            'I am getting refreshed every 3 seconds..! Random Number ==> ' + randomnumber);
+                }, 3000);
 });
