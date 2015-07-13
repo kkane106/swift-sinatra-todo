27c07d5594ab3fd7a215af24b243f9c6098c8dab
kkane106 <kkane106@gmail.com>
2015-07-13 18:32:14 -0400

    Add mobile_controller.rb for organization

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,24 +12,3 @@ get '/nothing_to_see' do
   "Nothing to see here. Move along."
 end
 
-post '/create_todo' do
-  values = JSON.parse(request.env["rack.input"].read)
-  if values["todos"]
-  	@todo = Todo.new(:text => values["todos"])
-    @todo.save
-      return {:todo => [@todo]}.to_json
-  else
-    return "error".to_json
-  end
-end
-
-
-post "/delete_todo" do
-  values = JSON.parse(request.env["rack.input"].read)
-  if values["todos"]
-    Todo.find_by_id(values["todos"]).destroy
-    return "success".to_json
-  else
-    return "error".to_json
-  end
-end
