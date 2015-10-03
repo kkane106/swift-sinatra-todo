e120ea5357ab6f49c0c9dafc2a9a5ffb5faba699
kkane106 <kkane106@gmail.com>
2015-10-02 19:53:53 -0600

    Comment out the refresh function on todos (it's getting in the way at the moment)

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,12 @@
-function executeQuery() {
-  $.ajax({
-    url: 'http://swift-sushi-json.herokuapp.com/todos',
-    success: function(data) {
-      var newContent = $(data);
-      $('#show').replaceWith(newContent);
-    }
-  });
-  setTimeout(executeQuery, 8000); // you could choose not to continue on failure...
+// function executeQuery() {
+//   $.ajax({
+//     url: 'http://swift-sushi-json.herokuapp.com/todos',
+//     success: function(data) {
+//       var newContent = $(data);
+//       $('#show').replaceWith(newContent);
+//     }
+//   });
+  // setTimeout(executeQuery, 8000); // you could choose not to continue on failure...
 }
 
 
@@ -16,7 +16,7 @@ function executeQuery() {
 
 $(document).ready(function() {
   // run the first time; all subsequent calls will take care of themselves
-  setTimeout(executeQuery, 8000);
+  // setTimeout(executeQuery, 8000);
 
   $(".todos").click(function(){
     $.get('/todos', function(data){
