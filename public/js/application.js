6a9d3955bede08db0ab993eaa68ce8594a30c6b5
kkane106 <kkane106@gmail.com>
2015-10-02 19:29:52 -0600

    Try some ajax

--- public/js/application.js
+++ public/js/application.js
@@ -9,7 +9,15 @@ function executeQuery() {
   setTimeout(executeQuery, 8000); // you could choose not to continue on failure...
 }
 
+
 $(document).ready(function() {
   // run the first time; all subsequent calls will take care of themselves
   setTimeout(executeQuery, 8000);
+
+  $("button").click(function(){
+    $.get('/todo/3', function(data){
+      $(".results").html(data);
+    });
+  });
 });
+
